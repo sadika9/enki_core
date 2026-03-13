857ab896e2b2df07cd8451e95083055c431118b6
dewmal <dewmal@syigen.com>
2026-03-14 01:10:41 +0530

    create macros

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+#[macro_use]
+mod macros;
+
 use serde_json::{json, Map, Value};
 use std::collections::BTreeMap;
 use std::env;
@@ -43,45 +46,6 @@ trait Tool {
             "parameters": self.parameters(),
         })
     }
-}
-
-macro_rules! define_tool {
-    (
-        $tool_type:ident,
-        name: $name:literal,
-        description: $description:literal,
-        parameters: $parameters:expr,
-        |$args:ident, $ctx:ident| $body:block
-    ) => {
-        struct $tool_type;
-
-        impl Tool for $tool_type {
-            fn name(&self) -> &'static str {
-                $name
-            }
-
-            fn description(&self) -> &'static str {
-                $description
-            }
-
-            fn parameters(&self) -> Value {
-                $parameters
-            }
-
-            fn execute(&self, $args: &Value, $ctx: &ToolContext) -> String $body
-        }
-    };
-}
-
-macro_rules! register_tools {
-    ($($tool_type:ident),+ $(,)?) => {{
-        let mut registry: ToolRegistry = BTreeMap::new();
-        $(
-            let tool: Box<dyn Tool> = Box::new($tool_type);
-            registry.insert(tool.name().to_string(), tool);
-        )+
-        registry
-    }};
 }
 
 define_tool!(
